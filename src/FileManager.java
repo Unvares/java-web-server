a60dc1ecf580170d3d15a294fecef26107b37c88
Rihards Okmanis <unvares@Rihardss-MacBook-Air.local>
2024-03-31 15:12:06 +0200

    feat: implement file manager

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,6 +1,8 @@
 package src;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class FileManager {
@@ -10,17 +12,33 @@ public class FileManager {
         this.rootDirectory = rootDirectory;
     }
 
-    public File getFile(String filePath) {
-        return new File("");
-        // Retrieve the file from the server's file system
+    public File getFile(String path) throws IOException {
+        File requestedFile = new File(rootDirectory, path);
+        validateFilePath(requestedFile);
+
+        if (!requestedFile.exists()) {
+            throw new IOException("Requested file does not exist.");
+        }
+
+        return requestedFile;
     }
 
-    public boolean fileExists(String filePath) {
-        return false;
-        // Check if a file exists at the given path
+    public void saveFile(InputStream fileData, String path) throws IOException {
+        File targetFile = new File(rootDirectory, path);
+        validateFilePath(targetFile);
+
+        FileOutputStream out = new FileOutputStream(targetFile);
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = fileData.read(buffer)) != -1) {
+            out.write(buffer, 0, bytesRead);
+        }
+        out.close();
     }
 
-    public void saveFile(InputStream fileData, String filePath) {
-        // Process the incoming byte stream and write it to a file
+    private void validateFilePath(File file) throws IOException {
+        if (!file.getCanonicalPath().startsWith(rootDirectory.getCanonicalPath())) {
+            throw new IllegalArgumentException("Access to files outside of the root directory is prohibited.");
+        }
     }
 }
