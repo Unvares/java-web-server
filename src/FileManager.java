116ba8f6b9a9d49e2a2df63b2bdd967decbb093d
Rihards Okmanis <unvares@Rihardss-MacBook-Air.local>
2024-03-31 20:41:04 +0200

    chore: fix spacing in project files

--- src/FileManager.java
+++ src/FileManager.java
@@ -10,60 +10,60 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 
 public class FileManager {
-    private final File rootDirectory;
+  private final File rootDirectory;
 
-    FileManager(File rootDirectory) {
-        this.rootDirectory = rootDirectory;
+  FileManager(File rootDirectory) {
+    this.rootDirectory = rootDirectory;
+  }
+
+  public File getContent(String path) throws IOException {
+    File requestedContent = new File(rootDirectory, path);
+    validatePath(requestedContent);
+
+    if (!requestedContent.exists()) {
+      throw new NoSuchFileException("Requested file does not exist.");
     }
 
-    public File getContent(String path) throws IOException {
-        File requestedContent = new File(rootDirectory, path);
-        validatePath(requestedContent);
+    return requestedContent;
+  }
 
-        if (!requestedContent.exists()) {
-            throw new NoSuchFileException("Requested file does not exist.");
-        }
+  public byte[] getContentAsBytes(String path) throws IOException {
+    File file = getContent(path);
+    byte[] data = Files.readAllBytes(file.toPath());
+    return data;
+  }
 
-        return requestedContent;
+  public String getMimeType(String path) throws IOException {
+    String mimeType = Files.probeContentType(Paths.get(path));
+    if (mimeType == null) {
+      throw new IllegalArgumentException("File extension is missing or MIME type could not be determined.");
     }
 
-    public byte[] getContentAsBytes(String path) throws IOException {
-        File file = getContent(path);
-        byte[] data = Files.readAllBytes(file.toPath());
-        return data;
+    return mimeType;
+  }
+
+  public void saveFile(InputStream fileData, String path) throws IOException {
+    File targetFile = new File(rootDirectory, path);
+    validatePath(targetFile);
+
+    FileOutputStream out = new FileOutputStream(targetFile);
+    byte[] buffer = new byte[1024];
+    int bytesRead;
+    while ((bytesRead = fileData.read(buffer)) != -1) {
+      out.write(buffer, 0, bytesRead);
     }
+    out.close();
+  }
 
-    public String getMimeType(String path) throws IOException {
-        String mimeType = Files.probeContentType(Paths.get(path));
-        if (mimeType == null) {
-            throw new IllegalArgumentException("File extension is missing or MIME type could not be determined.");
-        }
-
-        return mimeType;
+  private void validatePath(File file) throws IOException {
+    if (!file.getCanonicalPath().startsWith(rootDirectory.getCanonicalPath())) {
+      throw new AccessDeniedException("Access to files outside of the root directory is prohibited.");
     }
+  }
 
-    public void saveFile(InputStream fileData, String path) throws IOException {
-        File targetFile = new File(rootDirectory, path);
-        validatePath(targetFile);
-
-        FileOutputStream out = new FileOutputStream(targetFile);
-        byte[] buffer = new byte[1024];
-        int bytesRead;
-        while ((bytesRead = fileData.read(buffer)) != -1) {
-            out.write(buffer, 0, bytesRead);
-        }
-        out.close();
-    }
-
-    private void validatePath(File file) throws IOException {
-        if (!file.getCanonicalPath().startsWith(rootDirectory.getCanonicalPath())) {
-            throw new AccessDeniedException("Access to files outside of the root directory is prohibited.");
-        }
-    }
-
-    public boolean isDirectory(String path) throws IOException {
-        File file = new File(rootDirectory, path);
-        validatePath(file);
-        return file.isDirectory() ? true : false;
-    }
+  public boolean isDirectory(String path) throws IOException {
+    File file = new File(rootDirectory, path);
+    validatePath(file);
+    return file.isDirectory() ? true : false;
+  }
 }
--- src/RequestData.java
+++ src/RequestData.java
@@ -1,100 +1,124 @@
 package src;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 import src.enums.RequestMethod;
 
 public class RequestData {
-    private final FileManager fileManager;
+  private final FileManager fileManager;
 
-    private final String url;
-    private final RequestMethod method;
-    private final String protocol;
-    private final Map<String, String> headers;
-    private final Map<String, String> queryParams;
+  private final String url;
+  private final RequestMethod method;
+  private final String protocol;
+  private final Map<String, String> headers;
+  private final Map<String, String> queryParams;
+  private final byte[] body;
 
-    RequestData(InputStream inputStream, FileManager fileManager) throws IOException {
-        this.fileManager = fileManager;
+  RequestData(InputStream inputStream, FileManager fileManager) throws IOException {
+    this.fileManager = fileManager;
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-        String[] parameters = in.readLine().split(" ");
-        this.method = parseMethod(parameters[0]);
-        this.url = parseUrl(parameters[1]);
-        this.protocol = parameters[2];
-        this.headers = parseHeaders(in);
-        this.queryParams = parseQueryParams(in);
+    BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+    String[] parameters = in.readLine().split(" ");
+    this.method = parseMethod(parameters[0]);
+    this.url = parseUrl(parameters[1]);
+    this.protocol = parameters[2];
+    this.queryParams = parseQueryParams(in);
+    this.headers = parseHeaders(in);
+    this.body = parseBody(in);
+  }
+
+  private RequestMethod parseMethod(String method) {
+    switch (method) {
+      case "GET":
+        return RequestMethod.GET;
+      case "POST":
+        return RequestMethod.POST;
+      default:
+        throw new IllegalArgumentException("Invalid method: " + method);
+    }
+  }
+
+  private String parseUrl(String url) throws IOException {
+    if (fileManager.isDirectory(url)) {
+      return url.endsWith("/") ? url + "index.html" : url + "/index.html";
+    }
+    return url;
+  }
+
+  private Map<String, String> parseHeaders(BufferedReader in) throws IOException {
+    Map<String, String> headers = new HashMap<>();
+
+    String line;
+    while ((line = in.readLine()) != null && !line.isEmpty()) {
+      if (line.contains(":")) {
+        String[] header = line.split(": ");
+        headers.put(header[0], header[1]);
+      }
     }
 
-    private RequestMethod parseMethod(String method) {
-        switch (method) {
-            case "GET":
-                return RequestMethod.GET;
-            case "POST":
-                return RequestMethod.POST;
-            default:
-                throw new IllegalArgumentException("Invalid method: " + method);
-        }
+    return headers;
+  }
+
+  private Map<String, String> parseQueryParams(BufferedReader in) {
+    Map<String, String> queryParams = new HashMap<>();
+
+    if (this.url.contains("?")) {
+      String[] urlParts = this.url.split("\\?");
+      String query = urlParts[1];
+      for (String param : query.split("&")) {
+        String[] keyValue = param.split("=");
+        queryParams.put(keyValue[0], keyValue[1]);
+      }
     }
 
-    private String parseUrl(String url) throws IOException {
-        if (fileManager.isDirectory(url)) {
-            return url.endsWith("/") ? url + "index.html" : url + "/index.html";
-        }
-        return url;
+    return queryParams;
+  }
+
+  private byte[] parseBody(BufferedReader in) throws IOException {
+    String header = headers.get("Content-Length");
+    if (header == null)
+      return new byte[0];
+    int contentLength = Integer.parseInt(header);
+    ByteArrayOutputStream body = new ByteArrayOutputStream();
+    for (int i = 0; i < contentLength; i++) {
+      body.write(in.read());
     }
+    return body.toByteArray();
+  }
 
-    private Map<String, String> parseHeaders(BufferedReader in) throws IOException {
-        Map<String, String> headers = new HashMap<>();
+  public String getUrl() {
+    return this.url;
+  }
 
-        String line;
-        while ((line = in.readLine()) != null && !line.isEmpty()) {
-            if (line.contains(":")) {
-                String[] header = line.split(": ");
-                headers.put(header[0], header[1]);
-            }
-        }
+  public RequestMethod getMethod() {
+    return this.method;
+  }
 
-        return headers;
-    }
+  public String getProtocol() {
+    return this.protocol;
+  }
 
-    private Map<String, String> parseQueryParams(BufferedReader in) {
-        Map<String, String> queryParams = new HashMap<>();
+  public Map<String, String> getHeaders() {
+    return new HashMap<>(this.headers);
+  }
 
-        if (this.url.contains("?")) {
-            String[] urlParts = this.url.split("\\?");
-            String query = urlParts[1];
-            for (String param : query.split("&")) {
-                String[] keyValue = param.split("=");
-                queryParams.put(keyValue[0], keyValue[1]);
-            }
-        }
+  public Map<String, String> getQueryParams() {
+    return new HashMap<>(this.queryParams);
+  }
 
-        return queryParams;
-    }
+  public String getBodyAsString() {
+    return new String(body, StandardCharsets.UTF_8);
+  }
 
-    public String getUrl() {
-        return this.url;
-    }
-
-    public RequestMethod getMethod() {
-        return this.method;
-    }
-
-    public String getProtocol() {
-        return this.protocol;
-    }
-
-    public Map<String, String> getHeaders() {
-        return new HashMap<>(this.headers);
-    }
-
-    public Map<String, String> getQueryParams() {
-        return new HashMap<>(this.queryParams);
-    }
+  public byte[] getBodyAsBytes() {
+    return body;
+  }
 
 }
